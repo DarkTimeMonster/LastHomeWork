27359342dc9576a78088f1e2f93c3b8685bb42b6
77784228 <77784228@BELAR>
2024-05-21 21:36:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("Введите первое число - ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число - ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1}+{num2}={num1+num2}");
+            */
+
+            /*
+            Console.WriteLine("Введите первое число - ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число - ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите третье число - ");
+            int num3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1}+{num2}+{num3}={num1 + num2 + num3}");
+            */
+
+            /*
+            Console.WriteLine("Введите первое число - ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число - ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{num1:f1}*{num2:f1}={num1 * num2:f1}");
+            */
+
+            /*
+            Console.WriteLine("Введите первое число - ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число - ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine($"{num1:f3}/{num2:f3}={num1 / num2:f3}");
+            */
+            /*
+            Console.WriteLine("Введите первое число - ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число - ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{num1} + {num2}={num2} + {num1}");
+            */
+            /*
+            Console.WriteLine("Введите первое число - ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число - ");
+            int num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"a+b={num1}+{num2}={num1+num2}\ta-b={num1}-{num2}={num1-num2}\ta-b={num1}*{num2}={num1 * num2}");
+            */
+            /*
+            Console.Write("Как тебя зовут?");
+            string name = Console.ReadLine();
+            Console.Write("Сколько тебе лет?");
+            int age =int.Parse( Console.ReadLine());
+            Console.Write($"{name}, ты родился в {2009 - age} году");
+            Console.ReadKey();
+            */
+            /*
+            Console.Write("Номинал купюры: ");
+            int note = int.Parse(Console.ReadLine());
+            Console.Write("Количество купюр: ");
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Сумма денег: {note * count:f2}р.");
+            */
+
+            /*
+            Console.Write("Сторона квадрата равна = ");
+            double font1 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Периметр равен= {font1 * 4}");
+            */
+
+            /*
+            Console.Write("Стороны Прямоугольника равна = ");
+            double font1 = double.Parse(Console.ReadLine());
+            double font2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Периметр равен= {2 * (font1 + font2)}");
+            */
+
+            Console.ReadKey();
+        }
+    }
+}
